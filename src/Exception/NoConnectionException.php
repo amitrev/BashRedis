158cc6f1e3bc0800efd42c286d61741d2ba9d0cb
Atanas Mitrev <amitrev@gmail.com>
2021-07-01 17:37:56 +0300

    Release/new (#11)
    Official Version 1

---
+++ src/Exception/NoConnectionException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bash\Bundle\CacheBundle\Exception;
+
+use Throwable;
+
+class NoConnectionException extends \Exception
+{
+    public function __construct($message = 'Redis server connection faded away!', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
