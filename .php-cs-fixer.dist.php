dcca244eed46e37d0066b7cc5786c66945e566ef
Atanas Mitrev <atanas@smakmedia.com>
2021-07-14 10:51:00 +0300

    update csfixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,24 +1,13 @@
 <?php
 
-return PhpCsFixer\Config::create()
-    ->setRules([
-        '@Symfony' => true,
-        '@Symfony:risky' => true,
-        '@DoctrineAnnotation' => true,
-        'strict_param' => true,
-        'yoda_style' => false,
-        'concat_space' => ['spacing' => 'one'],
-        'compact_nullable_typehint' => true,
-        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
-        'multiline_comment_opening_closing' => true,
-        'array_indentation' => true,
-        'explicit_string_variable' => true,
-        'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
-    ])
-    ->setRiskyAllowed(true)
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->in(__DIR__ . '/src')
-            ->append([__FILE__])
-    )
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var', 'vendors'])
 ;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true
+    ])
+    ->setFinder($finder)
+    ;
